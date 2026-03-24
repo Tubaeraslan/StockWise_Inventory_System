fe9ad47576cb1377dd3da8911ab6345810be2616
tubaeraslan <tubaeraslan193@gmail.com>
2026-03-24 21:31:03 +0300

    fix: Remove unreachable code in App.js - clean up leftover default CRA template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,6 @@ import './App.css';
 
 function App() {
   return <Dashboard />;
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
 }
 
 export default App;
